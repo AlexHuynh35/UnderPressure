e4454b44eaff8ff6dd4dab751fe8059dd28cc4b8
Alex Huynh <alexh3925@gmail.com>
2026-01-02 23:53:47 -0800

    feat: flame blade

---
+++ Assets/Player/Abilities/Sword/FlameBlade/FlameBladeAttack.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "FlameBladeAttack", menuName = "Abilities/Justine/Sword/FlameBlade/FlameBladeAttack", order = 1)]
+public class FlameBladeAttack : Ability
+{
+    [Header("Ability Stats")]
+    public float damage;
+    public bool piercing;
+
+    [Header("Hitboxes")]
+    public float radius;
+    public int angle;
+    public int number;
+    public float speed;
+    public float lifetime;
+    public GameObject hitboxPrefab;
+
+    public override void OnRelease(EntityManager caster, Vector2 direction)
+    {
+        Effect movementEffect = new MovementEffect(boost: 0f, source: caster, allowedTags: EntityTag.Player);
+        movementEffect.OnEnter(caster);
+    }
+
+    public override void StartActive(EntityManager caster, Vector2 direction, float chargeTime)
+    {
+        for (int i = -(int)Mathf.Floor(number / 2); i <= (int)Mathf.Floor(number / 2); i++)
+        {
+            List<Effect> effects = new List<Effect>()
+            {
+                new DamageEffect(damage: damage, piercing: piercing, source: caster, allowedTags: EntityTag.Breakable | EntityTag.Enemy),
+            };
+            HitboxShape shape = new CircleShape(radius: radius);
+            Vector2 directionOffset = AbilityHelper.AddOffset(caster.orientation, angle / (number + 1), i);
+            HitboxMovement movement = new StraightMovement(speed: speed, direction: directionOffset);
+            HitboxManager attack = Instantiate(hitboxPrefab, caster.transform.position, Quaternion.Euler(0, 0, Mathf.Atan2(directionOffset.y, directionOffset.x) * Mathf.Rad2Deg)).GetComponent<HitboxManager>();
+            attack.Initialize
+            (
+                owner: caster.gameObject,
+                effects: effects,
+                shape: shape,
+                movement: movement,
+                lifetime: lifetime,
+                targetSelf: false,
+                destroyOnHit: true
+            );
+        }
+    }
+
+    public override void EndActive(EntityManager caster)
+    {
+        Effect movementEffect = new MovementEffect(boost: 1f, source: caster, allowedTags: EntityTag.Player);
+        movementEffect.OnEnter(caster);
+    }
+}
---
+++ Assets/Player/Abilities/Sword/FlameBlade/FlameBladeAttackFinal.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "FlameBladeAttackFinal", menuName = "Abilities/Justine/Sword/FlameBlade/FlameBladeAttackFinal", order = 2)]
+public class FlameBladeAttackFinal : Ability
+{
+    [Header("Ability Stats")]
+    public float damage;
+    public bool piercing;
+
+    [Header("Hitboxes")]
+    public float radius;
+    public float lifetime;
+    public GameObject hitboxPrefab;
+
+    public override void OnPress(EntityManager caster, Vector2 direction)
+    {
+        Effect movementEffect = new MovementEffect(boost: 0f, source: caster, allowedTags: EntityTag.Player);
+        movementEffect.OnEnter(caster);
+    }
+
+    public override void OnRelease(EntityManager caster, Vector2 direction)
+    {
+        Effect movementEffect = new MovementEffect(boost: 1f, source: caster, allowedTags: EntityTag.Player);
+        movementEffect.OnEnter(caster);
+    }
+
+    public override void StartActive(EntityManager caster, Vector2 direction, float chargeTime)
+    {
+        List<Effect> effects = new List<Effect>()
+        {
+            new DamageAreaEffect(damage: damage, piercing: piercing, rate: 0.25f, source: caster, allowedTags: EntityTag.Breakable | EntityTag.Enemy)
+        };
+        HitboxShape shape = new CircleShape(radius: radius);
+        HitboxMovement movement = new StationaryMovement();
+        HitboxManager attack = Instantiate(hitboxPrefab, caster.transform.position, Quaternion.Euler(0, 0, Mathf.Atan2(caster.orientation.y, caster.orientation.x) * Mathf.Rad2Deg)).GetComponent<HitboxManager>();
+        attack.Initialize
+        (
+            owner: caster.gameObject,
+            effects: effects,
+            shape: shape,
+            movement: movement,
+            lifetime: lifetime * chargeTime,
+            targetSelf: false,
+            destroyOnHit: false
+        );
+    }
+}
